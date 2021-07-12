6573d7417f0e2f92199c86abfb93240f314ef447
SebastianDochia <sebastian.dochia@uipath.com>
2021-07-12 14:55:06 +0300

    chore: refactored JS file into TS

--- 1. Basic Web Development/script.js
+++ 1. Basic Web Development/script.js
@@ -1,44 +1,34 @@
-const API_URL = "http://localhost:3000";
-const HEADERS = {
-    "Content-Type": "application/json",
+var API_URL = "http://localhost:3000";
+var HEADERS = {
+    "Content-Type": "application/json"
 };
-
-document.addEventListener("DOMContentLoaded", () => {
-    const form = document.getElementById('permissions-form');
-
-    const radios = document.querySelectorAll('input[name="preset"]');
-
-    const pmsBoxes = document.querySelectorAll('input[name="permissionPms"]');
-    const allBoxes = document.querySelectorAll('input[name="permissionAll"]');
-
-    const boxes = { pmsBoxes, allBoxes };
-
+document.addEventListener("DOMContentLoaded", function () {
+    var form = document.getElementById('permissions-form');
+    var radios = document.querySelectorAll('input[name="preset"]');
+    var pmsBoxes = document.querySelectorAll('input[name="permissionPms"]');
+    var allBoxes = document.querySelectorAll('input[name="permissionAll"]');
+    var boxes = { pmsBoxes: pmsBoxes, allBoxes: allBoxes };
     handleRadios(radios, boxes);
     formSubmission(form);
-
     handlePopUp();
 });
-
-handleRadios = (radios, boxes) => {
-    radios.forEach(radio => {
-        radio.addEventListener('change', event => {
-            console.log(event.target.value);
+function handleRadios(radios, boxes) {
+    var _this = this;
+    radios.forEach(function (radio) {
+        radio.addEventListener('change', function (event) {
             switch (event.target.value) {
                 case "All":
-                    changeBoxesState(boxes.pmsBoxes, true);
-                    changeBoxesState(boxes.allBoxes, true);
+                    _this.changeBoxesState(boxes.pmsBoxes, true);
+                    _this.changeBoxesState(boxes.allBoxes, true);
                     break;
-
                 case "Permissive":
-                    changeBoxesState(boxes.pmsBoxes, true);
-                    changeBoxesState(boxes.allBoxes, false);
+                    _this.changeBoxesState(boxes.pmsBoxes, true);
+                    _this.changeBoxesState(boxes.allBoxes, false);
                     break;
-
                 case "Necessary":
-                    changeBoxesState(boxes.pmsBoxes, false);
-                    changeBoxesState(boxes.allBoxes, false);
+                    _this.changeBoxesState(boxes.pmsBoxes, false);
+                    _this.changeBoxesState(boxes.allBoxes, false);
                     break;
-
                 default:
                     console.log("#ERROR");
                     break;
@@ -46,45 +36,35 @@ handleRadios = (radios, boxes) => {
         });
     });
 }
-
-formSubmission = (form) => {
-    form.addEventListener('submit', (event) => {
+function formSubmission(form) {
+    form.addEventListener('submit', function (event) {
         event.preventDefault();
-
-        const data = document.getElementById('data').checked;
-        const record = document.getElementById('record').checked;
-        const harvest = document.getElementById('harvest').checked;
-        const laugh = document.getElementById('laugh').checked;
-        const send = document.getElementById('send').checked;
-        const read = document.getElementById('read').checked;
-
-        const messageJSON = JSON.stringify({data, record, harvest, laugh, send, read});
-
+        var data = document.getElementById('data').checked;
+        var record = document.getElementById('record').checked;
+        var harvest = document.getElementById('harvest').checked;
+        var laugh = document.getElementById('laugh').checked;
+        var send = document.getElementById('send').checked;
+        var read = document.getElementById('read').checked;
+        var messageJSON = JSON.stringify({ data: data, record: record, harvest: harvest, laugh: laugh, send: send, read: read });
         fetch(API_URL, { method: 'POST', headers: HEADERS, body: messageJSON })
-            .then(response => response.json())
-            .then(data => console.log(data))
-            .catch((error) => console.error('Error:', error));
+            .then(function (response) { return response.json(); })
+            .then(function (data) { return console.log(data); })["catch"](function (error) { return console.error('Error:', error); });
     });
 }
-
-handlePopUp = () => {
-    const popUp = document.getElementsByClassName('preferences')[0];
-
-    popUp.addEventListener('click', () => {
+function handlePopUp() {
+    var popUp = document.getElementsByClassName('preferences')[0];
+    popUp.addEventListener('click', function () {
         popUp.classList.add('showing');
         popUp.classList.remove('hidden');
-
-        popUp.addEventListener('mouseleave', () => {
+        popUp.addEventListener('mouseleave', function () {
             popUp.classList.add('hidden');
             popUp.classList.remove('showing');
-
-            popUp.removeEventListener('mouseleave', () => {});
+            popUp.removeEventListener('mouseleave', function () { });
         });
     });
 }
-
-changeBoxesState = (boxes, state) => {
-    boxes.forEach(box => {
+function changeBoxesState(boxes, state) {
+    boxes.forEach(function (box) {
         box.checked = state;
     });
 }
