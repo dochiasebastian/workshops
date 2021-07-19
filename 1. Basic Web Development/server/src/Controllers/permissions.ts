ac984d63a1b2926f5b1b134f582a9fe6f2fd67db
SebastianDochia <sebastian.dochia@uipath.com>
2021-07-19 14:26:40 +0300

    chore: move all routes to their controller

--- 1. Basic Web Development/server/src/Controllers/permissions.ts
+++ 1. Basic Web Development/server/src/Controllers/permissions.ts
@@ -1,8 +1,8 @@
 import ErrorResponse from '../Util/ErrorResponse';
 import asyncHandler from '../Middleware/Async';
-import Permission from '../Model/Permissions';
+import Permission, { PermissionDocument } from '../Model/Permissions';
 
-export const create = asyncHandler(async (req: any, res: any) => {
+export const createPermission = asyncHandler(async (req: any, res: any) => {
     const { type, text, id } = req.body;
 
     const permission = await Permission.create({
@@ -17,4 +17,31 @@ export const create = asyncHandler(async (req: any, res: any) => {
     });
 });
 
+export const deletePermission = asyncHandler(async (req: any, res: any, next: any) => {
+    const permission: PermissionDocument = await Permission.findByIdAndDelete(req.body.id);
+
+    if (!permission) {
+        return next(new ErrorResponse(`Permission not found with id ${req.body.id}`, 404));
+    }
+
+    res.status(200).json({ success: true, data: {} });
+});
+
+export const getPermissions = asyncHandler(async (req: any, res: any) => {
+    const permissions = await Permission.find();
+
+    res.status(200).json({ success: true, data: permissions });
+});
+
+export const updatePermission = asyncHandler(async (req: any, res: any, next: any) => {
+    const permission = await Permission.findByIdAndUpdate(req.body.id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    });
+
+    if (!permission) {
+        return next(new ErrorResponse(`Permission not found with id ${req.body.id}`, 404));
+    }
+});
 
